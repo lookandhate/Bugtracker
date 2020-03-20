ed38c4868faa9b12b65649ad44fab70645db7dfb
SadnessPWNZ <princessselestiapwnz@mail.ru>
2020-03-20 21:32:05 +0400

    Registration form added

--- src/Forms.py
+++ src/Forms.py
@@ -7,4 +7,10 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Join!')
