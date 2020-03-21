a3e04e430b8729c4fcd418a41915d0fc213682f4
SadnessPWNZ <princessselestiapwnz@mail.ru>
2020-03-21 11:53:59 +0400

    Added registration

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import render_template
+import os
+import hashlib
+
+from flask import render_template, send_from_directory
 from flask import Flask
 from flask import redirect
 from flask import url_for
@@ -12,22 +15,27 @@ from src import Forms
 
 from data import db_session
 
+# Init flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my temp secret key'
 
+# Init login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+# Port, IP address and debug mode
 PORT, HOST = 8080, "127.0.0.1"
 DEBUG = True
 
 
+# Loading current user
 @login_manager.user_loader
 def load_user(user_id):
     session = db_session.create_session()
     return session.query(User).get(user_id)
 
 
+# Main page of app
 @app.route('/')
 @app.route('/index')
 def index():
@@ -35,6 +43,7 @@ def index():
     return render_template('base.html', title=title)
 
 
+# Login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     title = 'Login'
@@ -42,16 +51,17 @@ def login():
 
     if form.validate_on_submit():
         session = db_session.create_session()
-        user = session.query(User).filter(User.email == form.email.data).first()
+        user = session.query(User).filter(User.username == form.username.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             return redirect("/")
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
-    return render_template('login.html', title='Авторизация', form=form)
+    return render_template('login.html', title=title, form=form)
 
 
+# Logout page
 @app.route('/logout')
 @login_required
 def logout():
@@ -59,15 +69,45 @@ def logout():
     return redirect("/")
 
 
+# Registration page
 @app.route('/join', methods=['GET', 'POST'])
 def join():
     title = 'Join us'
+
+    # Registration form
     form = Forms.RegistrationForm()
 
+    # And finally, redirecting to index page
     if form.validate_on_submit():
+        # Creating Database Session
+        session = db_session.create_session()
+
+        # User object for database
+        user = User()
+
+        # Hashing password here
+        password_hash = hashlib.new('md5', bytes(form.password.data, encoding='utf8'))
+
+        # Filling database with user data
+        user.username = form.username.data
+
+        # Here we use not password but its hash
+        user.hashed_password = password_hash.hexdigest()
+
+        # Adding user to database
+        session.merge(user)
+        # Commiting changes
+        session.commit()
+
         return redirect('/index')
 
-    return render_template('login.html', title=title, form=form)
+    return render_template('join.html', title=title, form=form)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 if __name__ == '__main__':
