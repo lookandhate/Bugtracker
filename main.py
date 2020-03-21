8c5dd209cead5336c2d17cb204863968ce6ecc99
SadnessPWNZ <princessselestiapwnz@mail.ru>
2020-03-21 21:31:43 +0400

    Implement creating and viewing project

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import os
 import hashlib
 
-from flask import render_template, send_from_directory
+from flask import render_template, send_from_directory, flash, url_for, redirect
 from flask import Flask
-from flask import redirect
-from flask import url_for
 
 from flask_login import LoginManager
 from flask_login import login_user, login_required, logout_user, current_user
 
 # import logging
-from data.users import User
+from data.users import User, Project, Issue
 from src import Forms
 
 from data import db_session
@@ -87,8 +85,9 @@ def join():
         session.merge(user)
         # Commiting changes
         session.commit()
-
         session.close()
+
+        flash('Your account has been created and now you are able to log in', 'success')
         return redirect('/index')
     session.close()
     return render_template('join.html', title=title, form=form, registred_users=registred_users)
@@ -111,7 +110,6 @@ def login():
             print(current_user)
 
             session.close()
-            return redirect("/")
 
         session.close()
         return render_template('login.html',
@@ -146,7 +144,8 @@ def profile(user_id):
         date_of_reg = session.query(User.created_date).filter(User.id == user_id).first()[0]
 
         # Unpacking datetime.datetime object on time units
-        date_of_reg = f'{date_of_reg.day}-{date_of_reg.month}-{date_of_reg.year} at {date_of_reg.hour}:{date_of_reg.minute}:{date_of_reg.second}'
+        date_of_reg = f'{date_of_reg.day}-{date_of_reg.month}-{date_of_reg.year} at {date_of_reg.hour}:' \
+                      f'{date_of_reg.minute}:{date_of_reg.second}'
 
         session.close()
         return render_template('profile.html', title=f'{username}', id=user_id, username=username, projects=projects,
@@ -155,7 +154,94 @@ def profile(user_id):
 
     # If user doesn't exist, throw error page
     return render_template('error.html', error_code=404, error_message='User has been deleted or wasn`t registered',
-                           registred_users=registered_users, styles=url_for('static', filename='css/my_css.css'))
+                           registred_users=registered_users)
+
+
+@app.route('/profile/<user_id>/projects')
+@login_required
+def profile_projects(user_id):
+    # TODO: Implement user Projects
+    pass
+
+
+@app.route('/profile/<user_id>/issues')
+@login.required
+def profile_issues(user_id):
+    # TODO: Implement user Issues
+    pass
+
+
+
+@app.route('/project/new', methods=['GET', 'POST'])
+@login_required
+def new_project():
+    title = 'Create project'
+    creating_proj_form = Forms.CreateProject()
+    session = db_session.create_session()
+    registred_users = len(session.query(User).all())
+
+    if creating_proj_form.validate_on_submit():
+        if session.query(Project).filter(Project.project_name == creating_proj_form.project_name.data).all():
+            session.close()
+            return render_template('index.html',
+                                   message="Project with that name already created",
+                                   form=creating_proj_form)
+
+        project = Project()
+        project.project_name = creating_proj_form.project_name.data
+        project.members = f'{current_user.username}:root;'
+
+        # Commiting changes
+        session.merge(project)
+        session.commit()
+        session.close()
+
+        # Re-creating session to update DB state
+        session = db_session.create_session()
+        proj_id = \
+            session.query(Project.id).filter(Project.project_name == creating_proj_form.project_name.data).first()[0]
+        session.close()
+        return redirect(f'/projects/{proj_id}')
+    session.close()
+    return render_template('new_project.html', title=title, form=creating_proj_form, registred_users=registred_users)
+
+
+@app.route('/projects/<id>')
+@login_required
+def project(id):
+    # Creating db session
+    session = db_session.create_session()
+
+    registered_users = len(session.query(User).all())
+
+    # Get all project members
+    project_members = session.query(Project.members).filter(Project.id == id).first()
+
+    # Check does project exist
+    # If not then throw error page
+    if project_members is None:
+        return render_template('error.html', error_code=404, error_message='This project doesn`t exist',
+                               registred_users=registered_users)
+
+    # Check does current user have access to this project
+    # If don`t, throw error page
+    if str(current_user.username) not in project_members[0] or current_user.role != 'Admin':
+        return render_template('error.html', error_code=403, error_message='You don`t have access to this project')
+
+    # If project exist and user have access to it, then return project page
+    _project = session.query(Project).filter(Project.id == id).first()
+    date_of_creation = _project.created_date
+    date_of_creation = f'{date_of_creation.day}-{date_of_creation.month}-{date_of_creation.year} at {date_of_creation.hour}:' \
+                       f'{date_of_creation.minute}:{date_of_creation.second}'
+
+    project_members = str(_project.members).split(';')
+    if '' in project_members:
+        project_members.pop(project_members.index(''))
+
+    return render_template('project.html', id=id, project_name=_project.project_name,
+                           project_members=project_members,
+                           project_issues=str(_project.issues).split(';'),
+                           date_of_creation=date_of_creation)
 
 
 @app.route('/favicon.ico')
