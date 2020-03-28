2184370d1ef2b26563dbf759f228110f6ef2ed6b
SadnessPWNZ <princessselestiapwnz@mail.ru>
2020-03-28 20:35:01 +0400

    Add password and project properties changer form

--- src/Forms.py
+++ src/Forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, F
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class LoginForm(FlaskForm):
@@ -11,17 +11,37 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
+class ChangeProfileProperties(FlaskForm):
+    old_password = PasswordField('Old password', validators=[DataRequired(), Length(min=1, max=20)])
+    new_password = PasswordField('New password', validators=[DataRequired(), Length(min=1, max=20)])
+    new_password_repeat = PasswordField('New password', validators=[DataRequired(),
+                                                                    EqualTo('new_password',
+                                                                            message='Passwords must match'),
+                                                                    Length(min=1, max=20)])
+
+
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=1, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=1, max=20)])
     submit = SubmitField('Join!')
 
 
 class CreateProject(FlaskForm):
-    project_name = StringField('Project name', validators=[DataRequired()])
-    project_members = StringField('Type new project members with ; as separator')
+    project_name = StringField('Project name', validators=[DataRequired(), Length(min=1, max=20)])
+    short_project_tag = StringField('Short project name,if not specified, then generated automatically',
+                                    validators=[Length(max=5)])
+    project_description = StringField('Project description', validators=[Length(max=255)])
     submit = SubmitField('Create Project!')
 
 
 class CreateIssue(FlaskForm):
     pass
+
+
+class ChangeProjectProperties(FlaskForm):
+    project_name = StringField('Project name', validators=[DataRequired(), Length(min=1, max=20)])
+    short_project_tag = StringField('Short project name,if not specified, then generated automatically',
+                                    validators=[Length(max=5)])
+    project_description = StringField('Project description', validators=[Length(max=255)])
+    project_owner = SelectField(coerce=int)
+    submit = SubmitField('Save!')
