b6593438860432d3e5a71dbac6bc42aec399fe34
SadnessPWNZ <princessselestiapwnz@mail.ru>
2020-04-01 13:17:41 +0400

    1)Add description,STR(WTR) and summary info to issue 2)Add get attachments function to Issue Class

--- data/models.py
+++ data/models.py
@@ -2,9 +2,11 @@ import datetime
 import sqlalchemy
 import hashlib
 
+from typing import Iterable
+
 from .db_session import SqlAlchemyBase, create_session
 
-from sqlalchemy import orm, select
+from sqlalchemy import orm, select, insert
 from flask_login import UserMixin
 
 association_table_user_to_issue = sqlalchemy.Table('user_to_issue', SqlAlchemyBase.metadata,
@@ -34,6 +36,17 @@ association_table_subsystems_to_project = sqlalchemy.Table('subsystems_to_projec
                                                                              sqlalchemy.ForeignKey('projects.id')),
                                                            sqlalchemy.Column('subsystem', sqlalchemy.String)
                                                            )
+association_table_file_to_issue = sqlalchemy.Table('file_to_issue', SqlAlchemyBase.metadata,
+                                                   sqlalchemy.Column('issue_id', sqlalchemy.Integer,
+                                                                     sqlalchemy.ForeignKey('issues.id')),
+                                                   sqlalchemy.Column('file_path', sqlalchemy.String)
+                                                   )
+
+association_table_priority_to_project = sqlalchemy.Table('priority_to_project', SqlAlchemyBase.metadata,
+                                                         sqlalchemy.Column('project_id', sqlalchemy.Integer,
+                                                                           sqlalchemy.ForeignKey('projects.id')),
+                                                         sqlalchemy.Column('priority', sqlalchemy.String)
+                                                         )
 
 
 class User(SqlAlchemyBase, UserMixin):
@@ -63,15 +76,15 @@ class User(SqlAlchemyBase, UserMixin):
                                      default=datetime.datetime.now)
     role = sqlalchemy.Column(sqlalchemy.String, default='User')
 
-    issues = orm.relationship('Issue',
-                              secondary=association_table_user_to_issue,
-                              backref='assignees')
+    issues = orm.relation('Issue',
+                          secondary=association_table_user_to_issue,
+                          backref='assignees')
     projects = orm.relation('Project',
                             secondary=association_table_user_to_project,
                             backref='members')
 
     def __repr__(self):
-        return f'username={self.username}; id={self.id}'
+        return f'username={self.username}; id={self.id};role={self.role};\nprojects {self.projects}\nissues:{self.issues}'
 
     def __str__(self):
         return self.__repr__()
@@ -95,6 +108,7 @@ class User(SqlAlchemyBase, UserMixin):
                 association_table_user_to_project.c.member_id == self.id).where(
                 association_table_user_to_project.c.project_id == project_id)
         ).fetchone()['project_role']
+        session.close()
         return result
 
     def project_date_of_add(self, project_id):
@@ -134,7 +148,8 @@ class Project(SqlAlchemyBase):
     issues = orm.relation('Issue', secondary=association_table_project_to_issue, backref='project_issues')
 
     def __repr__(self):
-        return f'Project name={self.project_name}; id={self.id}; root={self.get_root()}\ndesc:'
+        return f'Project name= {self.project_name}; id= {self.id}; root= {self.get_root().username}\ndesc: {self.description}' \
+               f'\nmembers: {len(self.members)}\nissues: {len(self.issues)}'
 
     def __str__(self):
         return self.__repr__()
@@ -142,11 +157,37 @@ class Project(SqlAlchemyBase):
     def get_project_subsystems(self):
         session = create_session()
         result = session.execute(
-            select[association_table_subsystems_to_project.c.subsystem].where(
+            select([association_table_subsystems_to_project.c.subsystem]).where(
                 association_table_subsystems_to_project.c.project_id == self.id
             )
-        ).fetchall()['subsystem']
+        ).fetchall()
+        session.close()
         return result
+
+    def add_project_subsystems(self, subsystems: Iterable):
+        session = create_session()
+        for subsystem in subsystems:
+            session.execute(association_table_subsystems_to_project.insert().values(self.id, subsystem))
+        session.commit()
+        session.close()
+
+    def get_project_priorities(self):
+        session = create_session()
+        result = session.execute(
+            select([association_table_priority_to_project.c.priority]).where(
+                association_table_priority_to_project.c.project_id == self.id
+            )
+        ).fetchall()
+        session.close()
+        return result
+
+    def add_project_priorities(self, id: int, priorities: Iterable):
+        session = create_session()
+        for priority in priorities:
+            session.execute(association_table_priority_to_project.insert(),
+                            {'project_id': id, 'priority': priority})
+        session.commit()
+        session.close()
 
     def get_root(self):
         """
@@ -155,13 +196,15 @@ class Project(SqlAlchemyBase):
         """
         session = create_session()
         member_id = session.execute(
-            select[association_table_user_to_project.c.member_id].where(
+            select([association_table_user_to_project.c.member_id]).where(
                 association_table_user_to_project.c.project_id == self.id
             ).where(
                 association_table_user_to_project.c.project_role == 'root'
             )
-        ).fetchone()['member_id']
-        user = session.query(User).filter(User.id == member_id)
+        ).fetchone()
+        member_id = member_id['member_id']
+        user = session.query(User).filter(User.id == member_id).first()
+        session.close()
         return user
 
 
@@ -179,9 +222,25 @@ class Issue(SqlAlchemyBase):
     tracking = sqlalchemy.Column(sqlalchemy.String, unique=True)
     priority = sqlalchemy.Column(sqlalchemy.String, index=True)
     state = sqlalchemy.Column(sqlalchemy.String)
+    description = sqlalchemy.Column(sqlalchemy.String)
+    steps_to_reproduce = sqlalchemy.Column(sqlalchemy.String)
+    summary = sqlalchemy.Column(sqlalchemy.String)
 
     def __repr__(self):
-        return f'Issue name={self.tracking}; id={self.id}'
+        return f'Issue name={self.tracking}; id={self.id}\ndesc: {self.description}'
 
     def __str__(self):
         return self.__repr__()
+
+    def get_attachments(self):
+        """
+        :return: list of attachments path
+        """
+        session = create_session()
+        result = session.execute(
+            select([association_table_file_to_issue.c.file_path]).where(
+                association_table_file_to_issue.c.issue_id == self.id
+            )
+        ).fetchall()['file_path']
+        session.close()
+        return result
