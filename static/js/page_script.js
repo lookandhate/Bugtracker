68c11a2c96c35e20487cc8407993a55524063a0e
SadnessPWNZ <princessselestiapwnz@mail.ru>
2020-04-06 13:50:12 +0400

    js-script for adding new user

---
+++ static/js/page_script.js
@@ -0,0 +1,11 @@
+// document.getElementById("new_member").addEventListener("click",function() { get_Username(project_id);}, false);
+
+
+function get_Username(project_id) {
+  var username = prompt("Enter username ");
+  if(username === null) {
+    return;
+  }
+  window.location.href = "/projects/" + project_id + "/manage/add_member/?username=" +  username;
+  console.log(project_id);
+}
