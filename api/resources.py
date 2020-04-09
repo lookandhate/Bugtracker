4c51d85a0a04a40f2135c1402560334cbbacf030
SadnessPWNZ <princessselestiapwnz@mail.ru>
2020-04-09 15:28:24 +0400

    Made post query from Project creation

--- api/resources.py
+++ api/resources.py
@@ -57,36 +57,77 @@ class UserResourceList(Resource):
 class ProjectResource(Resource):
     project_creation_args = reqparse.RequestParser()
     project_creation_args.add_argument('API_KEY', required=True)
-    project_creation_args.add_argument('project_name', required=True)
-    project_creation_args.add_argument('description', required=True)
+    project_creation_args.add_argument('project_name', required=False)
+    project_creation_args.add_argument('description', required=False)
     project_creation_args.add_argument('short_tag', required=False)
 
     def get(self, project_id):
         abort_if_not_found(Project, project_id)
         session = create_session()
+        # Get user object by given API KEY
+        user_object = session.query(User).filter(User.API_KEY ==
+                                                 self.project_creation_args.parse_args(['API_KEY'])).first()
+        # If user doesn't exist -> throw 404 with message
+        if user_object is None:
+            abort(404, message=f'User with api key {self.project_creation_args.parse_args(["API_KEY"])} not found')
+
         project = session.query(project_id).get()
+
+        # Check if user in project_members
+        # If not -> throw 403
+        if user_object not in project.members:
+            abort(403, message="You don't have access to this project")
+
         return jsonify({'project': project.to_dict(
             only=('description', 'short_project_tag', 'project_name')
         )
         })
 
-    def post(self):
+    def post(self, project_id=0):
         session = db_session.create_session()
         args = self.project_creation_args.parse_args()
         if 'short_tag' in args:
             short_tag = args['short_tag']
         else:
             short_tag = args['project_name'][:4]
-        project_object = session.query(Project).filter(Project.project_name == args['project_name'])
+
+        # Get user object by given API KEY
+        user_object = session.query(User).filter(User.API_KEY ==
+                                                 args['API_KEY']).first()
+        # If user doesn't exist -> throw 404 with message
+        if user_object is None:
+            abort(404, message=f'User with api key {self.args["API_KEY"]} not found')
+
+        project_object = session.query(Project).filter(Project.project_name == args['project_name']).first()
+
+        # Check if project with that project name exist
         if project_object is not None:
             abort(409, message='Project with this project name already exists')
+        # Check if project with that project short tag exist
+        if not session.query(Project).filter(Project.short_project_tag == args['short_tag']).first() is None:
+            abort(409, message='Project with this project short-tag already exists')
+
         project_object = Project(
             project_name=args['project_name'], description=args['description'],
-            short_project_tag=args['short_project_tag']
-        )
+            short_project_tag=args['short_tag'])
+        project_object.members.append(user_object)
+
+        # Commiting changes
+        session.merge(project_object)
+        session.commit()
+
+        upd = association_table_user_to_project.update().values(project_role='root').where(
+            association_table_user_to_project.c.member_id == user_object.id).where(
+            association_table_user_to_project.c.project_id == project_object.id)
+        project_id = project_object.id
+        project_object.add_project_priorities(project_id, ('Critical', 'Major', 'Minor', 'Normal'))
+        session.execute(upd)
+        session.commit()
+        session.close()
         session.add(project_object)
         session.commit()
-        return jsonify({'success': 'OK'})
+        return jsonify({'success': 'OK',
+                        'id': f'{project_id}'})
 
 
 class ProjectResourceList(Resource):
