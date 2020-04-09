7f26607d1e70e16514b46c85d2cb9f68a7133481
SadnessPWNZ <princessselestiapwnz@mail.ru>
2020-04-09 15:31:38 +0400

    Implement generate_api_key function

---
+++ src/misc_funcs.py
@@ -0,0 +1,13 @@
+import string
+import random
+
+letters = string.ascii_uppercase
+
+
+def generate_api_key(length: int = 24) -> str:
+    # Default len of api key is 24 symbols
+    new_api_key = ''
+    for i in range(length):
+        new_api_key += random.choice(letters)
+
+    return new_api_key
