c984ba85d4d29c378fb7a24403699a8914695d09
SadnessPWNZ <princessselestiapwnz@mail.ru>
2020-04-09 18:47:43 +0400

    Changed default i iterator on _

--- src/misc_funcs.py
+++ src/misc_funcs.py
@@ -7,7 +7,7 @@ letters = string.ascii_uppercase
 def generate_api_key(length: int = 24) -> str:
     # Default len of api key is 24 symbols
     new_api_key = ''
-    for i in range(length):
+    for _ in range(length):
         new_api_key += random.choice(letters)
 
     return new_api_key
