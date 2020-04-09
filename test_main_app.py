0e8f91948afb00e9ee4a4babc2b2aca352d26a5e
SadnessPWNZ <princessselestiapwnz@mail.ru>
2020-04-09 10:56:40 +0400

    Add some test cases

--- test_main_app.py
+++ test_main_app.py
@@ -1,14 +1,20 @@
 import pytest
 from main_app import app
 
+testing_app = app.test_client()
 
-class TestProjectGET:
-    app = app.test_client()
 
+class TestConnectionToWebSite:
+    def test_connection(self):
+        result = testing_app.get('/')
+        assert result.status_code == 200
+
+
+class TestUserGET:
     def test_get_404(self):
-        result = self.app.get('/api/v0/user/-1')
+        result = testing_app.get('/api/v0/user/-1')
         assert result.status_code == 404
 
     def test_get_200(self):
-        result = self.app.get('/api/v0/user/1')
+        result = testing_app.get('/api/v0/user/1')
         assert result.status_code == 200
