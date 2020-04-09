be32f6a5dbf0b5548ad53f973071e9c8c0bbe09e
SadnessPWNZ <princessselestiapwnz@mail.ru>
2020-04-09 10:57:18 +0400

    Changed import of app object

--- src/errors.py
+++ src/errors.py
@@ -1,17 +1,18 @@
-from main import app
+from main_app import app
 from flask import render_template
 
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    print(404)
+    return render_template('errors/404.html'), 404
 
 
 @app.errorhandler(403)
 def access_restricted(error):
-    return render_template('403.html'), 403
+    return render_template('errors/403.html'), 403
 
 
 @app.errorhandler(422)
 def unprocessable_entity(error):
-    return render_template('422.html'), 422
+    return render_template('errors/422.html'), 422
