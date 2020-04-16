5bb70c97aa1a3ffcc95c9970a92bf1a5cf1be32f
SadnessPWNZ <princessselestiapwnz@mail.ru>
2020-04-16 17:25:16 +0400

    Implemented Issue requests

--- api/resources.py
+++ api/resources.py
@@ -145,6 +145,9 @@ class UserResourceList(Resource):
 
 
 class ProjectResource(Resource):
+    """
+    Implements interaction with Project entity by API
+    """
     project_request_args = reqparse.RequestParser()
     project_request_args.add_argument('API_KEY', required=True)
     project_request_args.add_argument('project_id', required=False)
@@ -188,6 +191,16 @@ class ProjectResource(Resource):
         })
 
     def post(self):
+        """
+        Implementation of creating project POST request
+        Creating project if API KEY, project name, project_description
+        and project tag are correct and assigns it on requested user
+
+        :return: 200 and Project.id if project created correctly
+        400 if some if necessary arguments have not been passed
+        401 if BAD api key been passed
+        409 If project with that project name or project tag already exist
+        """
         session = db_session.create_session()
         args = self.project_request_args.parse_args()
         # Check if project name and project description passed
@@ -277,3 +290,188 @@ class ProjectResourceList(Resource):
             only=('description', 'short_project_tag', 'project_name', 'root')
         ) for item in projects]
         })
+
+
+class IssueResource(Resource):
+    """
+    Implements interaction with Issue entity by API
+    """
+    issue_parser = reqparse.RequestParser()
+    issue_parser.add_argument('API_KEY', required=True)
+    issue_parser.add_argument('tag', reuired=False)
+
+    issue_parser.add_argument('project_id', required=False)
+    issue_parser.add_argument('summary', required=False)
+    issue_parser.add_argument('steps_to_reproduce', required=False)
+    issue_parser.add_argument('description', required=False)
+    issue_parser.add_argument('state', required=False)
+    issue_parser.add_argument('priority', required=False)
+
+    def get(self):
+        """
+        Implementation of GET request to Issue property
+        :return: JSON with Issue properties if there is no errors
+        Otherwise, 404 if issue does not exist
+        403 if user doesnt have access to this Issue
+        401 if API key is incorrect
+        400 if one of necessary have not been passed
+        """
+        args = self.issue_parser.parse_args()
+
+        # Check if project_if in args
+        if 'tag' not in args.keys():
+            app.logger.info(f'GET to IssueResource, tag has not been passed')
+            abort(400, message='You did not pass the tag')
+
+        # Check if Issue with given tag exists
+        abort_if_not_found(Issue, args['tag'])
+
+        session = create_session()
+        # Requested user here means user that own API key
+        requested_user: Optional[User] = session.query(User).filter(User.API_KEY ==
+                                                                    args["API_KEY"]).first()
+
+        # If requested user doesn't exist -> throw 401 with message
+        if args['API_KEY'] == 'None' or requested_user is None:
+            app.logger.info(f'GET to IssueResource passed bad API_KEY')
+            abort(401, message=f'You passed bad API key')
+
+        issue_object: Optional[Issue] = session.query(Issue).filter(Issue.tracking == args['tag']).first()
+        # Check if user has access to the Issue
+        if requested_user not in Issue.project.members and not requested_user.is_Admin:
+            app.logger.info(f'GET to IssueResource, user with given API KEY doesnt have access to project')
+            abort(403, message="You don't have access to this project")
+
+        return jsonify({'issue': issue_object.to_dict(
+            only=('tracking', 'priority', 'state', 'description', 'steps_to_reproduce', 'summary', 'project_name',
+                  'assign_on'))})
+
+    def post(self):
+        """
+        Implementation of creating Issue POST request
+        Creating issue if all arguments passed, correct and assigns it on requested user
+
+        :return: 200 and Issue.tag if Issue created successfully
+        400 if some if necessary arguments have not been passed
+        401 if BAD api key been passed
+        """
+
+        session = db_session.create_session()
+        args = self.project_request_args.parse_args()
+        # Check if project name and project description passed
+        if 'project_id' not in args.keys() or 'summary' not in args.keys() or 'steps_to_reproduce' not in args.keys() or 'description' not in args.keys() \
+                or 'state' not in args.keys() or 'priority' not in args.keys():
+            app.logger.info(
+                'POST to IssueResource, 400, NotEnoughArguments ')
+
+            abort(400, message='You did'
+                               ' not passe one(or more) of requirement arguments'
+                               ' (project_id,summary,steps_to_reproduce,description,state,priority))')
+
+        # Get user object
+        requested_user: Optional[User] = session.query(User).filter(User.API_KEY == args['API_KEY']).first()
+        # Check if user exists
+        if requested_user is None or args['API_KEY'] == 'None':
+            abort(401, message=f'You passed bad API key')
+
+        # Check if Project exists and user has access to it
+        app.logger.info(f'Check if project with project_id={args["project_id"]} exists')
+        abort_if_not_found(Project, args['project_id'])
+
+        app.logger.info(f'Project exist, check if user has access to it')
+        # Project exists
+        project_object = session.query(Project).filter(Project.id == args["project_id"]).first()
+
+        # Check if user has access to it
+        if requested_user not in project_object.members and not requested_user.is_admin:
+            app.logger.info(f'User {requested_user.username} doesnt have access to Project(project_name='
+                            f'{project_object.project_name}, id={args["project_id"]}')
+            abort(403, message="You don't have access to this project")
+
+        issue_object = Issue
+        all_issues = len(project_object.issues)
+        issue_object.tracking = f'{project_object.short_project_tag}-{all_issues + 1}'
+        issue_object.summary = args['summary']
+        issue_object.priority = args['priority']
+        issue_object.state = args['state']
+        issue_object.description = args['description']
+        issue_object.steps_to_reproduce = args['steps_to_reproduce']
+        issue_object.project_id = args['project_id']
+
+        project_object.issues.append(issue_object)
+
+        issue_tag = issue_object.tracking
+        session.merge(issue_object)
+        session.commit()
+        session.refresh(issue_object)
+        issue_object.assignees.append(requested_user)
+        session.commit()
+        return jsonify({'success': 'OK',
+                        'tag': f'{issue_tag}'})
+
+
+class IssueResourceList(Resource):
+    """
+    Implements get request to all Issues
+    """
+    issues_list_parser = reqparse.RequestParser()
+    issues_list_parser.add_argument('API_KEY', required=True)
+    issues_list_parser.add_argument('project_id', required=False)
+
+    def get(self):
+        """
+        Implementation of GET request to list of Issues
+        :return: JSON with list of Issues if there is no errors
+        Otherwise, 404 if issue does not exist
+        403 if user doesnt have access to this Issue
+        401 if API key is incorrect
+        """
+
+        args = self.issues_list_parser.parse_args()
+        session = db_session.create_session()
+        app.logging.info(f'GET IssueResourceList with args{args}')
+        # Check if user exists
+        requested_user: Optional[User] = session.query(User).filter(User.API_KEY == args['API_KEY']).first()
+        if requested_user is None or args['API_KEY'] == 'None':
+            app.logging.info(f'GET IssueResourceList User with API key {args["API_KEY"]} doesnt exist, return 400')
+            abort(400)
+        # If project_id has not been passed that's mean API should return all issues
+        if 'project_id' not in args.keys():
+            # But list with all issues can access ONLY site admin
+            # Check if requested user is admin
+            if not requested_user.is_admin:
+                app.logging.info(f'GET IssueResourceList without project_id,'
+                                 f' {requested_user.username} not an ADMIN, abort with 403')
+                abort(403, message='Only admin can access to list of all Issues')
+
+            issues = session.query(Issue).all()
+            app.logging.info(f'GET IssueResourceList User({requested_user.username})'
+                             f' is admin, return list with all issues')
+
+            return jsonify({'issues': [item.to_dict(
+                only=('tracking', 'priority', 'state', 'description', 'steps_to_reproduce', 'summary', 'project_name',
+                      'assign_on')
+            ) for item in issues]
+            })
+
+        else:
+            # Check if project exists
+            app.logging.info(f'GET IssueResourceList checking if project exists ')
+            abort_if_not_found(Project, args['project_id'])
+            project_object: Project = session.query(Project).filter(Project.id == args["project_id"])
+            app.logging.info('Project exists, check if requested user has access to it')
+
+            # Check if requested user has access to project
+            if not requested_user.is_admin or requested_user not in project_object.members:
+                app.logging.info(f'GET IssueResourceList, {requested_user}'
+                                 f' is admin={requested_user.is_admin},'
+                                 f' has access to project={requested_user in project_object.members}')
+            app.logging.info(f'Response to {requested_user.username} with the list of Issues')
+            # GET all project issues
+            issues = project_object.issues
+            # And, return list in response
+            return jsonify({'issues': [item.to_dict(
+                only=('tracking', 'priority', 'state', 'description', 'steps_to_reproduce', 'summary', 'project_name',
+                      'assign_on')
+            ) for item in issues]
+            })
